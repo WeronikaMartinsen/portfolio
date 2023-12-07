72fab7dac6383e84a0eac5c9617053836ce81d6d
WeronikaMartinsen <121826608+WeronikaMartinsen@users.noreply.github.com>
2023-12-07 23:18:42 +0100

    fixed navbar

--- js/navbar.js
+++ js/navbar.js
@@ -1,12 +1,26 @@
 const navbar = document.querySelector(".navbar");
 
-navbar.innerHTML = `<div class="nav"><img src="/images/portfolioLogo.png"/>
+navbar.innerHTML = `<div class="nav">
+<img src="/images/martinsenLogo.png" alt="Logo">
 </div>
-<div><ol class="navList"><li>Home</li><li class="dropdown">Projects<div class="dropdown-content">
-<a href="https://celebrated-chebakia-e82c27.netlify.app/" class="dropdown-item">Web & Dev Parenthood</a>
-<a href="https://classy-rabanadas-8c360c.netlify.app/" class="dropdown-item">Rainy-Days</a>
-<a href="https://symphonious-marigold-52e292.netlify.app/" class="dropdown-item">Science Museum</a>
-</div></li><li>Contact</li><li>About</li></ol></div>
+<div class="menu"><i class="fa-solid fa-bars"></i>
+<i id="hidden" class="fa-solid fa-rectangle-xmark close-button"></i></div>
+<div class="navListContainer">
+<ul class="navList">
+  <li class="dropdown">
+    Projects
+    <div class="dropdown-content">
+      <a href="https://celebrated-chebakia-e82c27.netlify.app/" class="dropdown-item">Web & Dev Parenthood</a>
+      <a href="https://classy-rabanadas-8c360c.netlify.app/" class="dropdown-item">Rainy-Days</a>
+      <a href="https://symphonious-marigold-52e292.netlify.app/" class="dropdown-item">Science Museum</a>
+    </div>
+  </li>
+  <li>Home</li>
+  <li>Contact</li>
+  <li>About</li>
+</ul>
+</div>
+</div>
 
 `;
 
@@ -20,5 +34,24 @@ document.querySelector(".navList").addEventListener("click", function (event) {
   } else {
     // Close the dropdown if clicking outside
     dropdown.classList.remove("active"); // Remove the "active" class on .dropdown
+  }
+});
+
+const dropdown = document.querySelector(".dropdown");
+const dropdownContent = document.querySelector(".dropdown-content");
+const navList = document.querySelector(".navList");
+const menuButton = document.querySelector(".fa-bars");
+const closeButton = document.querySelector(".fa-rectangle-xmark");
+
+document.querySelector(".menu").addEventListener("click", function () {
+  const isActive = navList.classList.toggle("active");
+
+  // Toggle between menu and close icons
+  if (isActive) {
+    menuButton.style.display = "none";
+    closeButton.style.display = "inline-block";
+  } else {
+    menuButton.style.display = "inline-block";
+    closeButton.style.display = "none";
   }
 });
