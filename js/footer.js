4f230063a0f9bf13bbdf1cd56e4abe1ca74626d0
WeronikaMartinsen <121826608+WeronikaMartinsen@users.noreply.github.com>
2023-12-08 15:21:25 +0100

    fixed responsivness for footer

--- js/footer.js
+++ js/footer.js
@@ -3,10 +3,10 @@ const footer = document.querySelector(".footer");
 footer.innerHTML = `<div class="row">
 
 
-<div class="column"><img src="/images/martinsenLogo.png" alt="Logo">
+<div class="column firstOne"><img src="/images/martinsenLogo.png" alt="Logo">
 <a href=""><button class="button-main">CONTACT ME</button></a>
 </div>
-<div class="column">
+<div class="column border-left">
 <ul>
 <li>Home</li>
 <li>Contact</li>
@@ -14,7 +14,7 @@ footer.innerHTML = `<div class="row">
 <li>Project</li>
 </ul>
 </div>
-<div class="column">
+<div class="column border-left">
 <i class="fa-brands fa-github"></i>
 <i class="fa-brands fa-linkedin"></i>
 <i class="fa-brands fa-twitter"></i>
