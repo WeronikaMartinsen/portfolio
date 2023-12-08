093f6db17669a916ba14598dc3b823240fd097b4
WeronikaMartinsen <121826608+WeronikaMartinsen@users.noreply.github.com>
2023-12-08 20:16:00 +0100

    adding personality to the page

---
+++ js/contact.js
@@ -0,0 +1,6 @@
+const contact = document.querySelector(".contact");
+
+contact.innerHTML = `<div class="contact"><h3>Get in touch!</h3>
+<span>Feel free to reach out! I'm always open to new opportunities and collaborations.</span>
+<span class="black">wb3167@gmail.com</span>
+<button class="button-secondary no-border">Contact me</button></div>`;
