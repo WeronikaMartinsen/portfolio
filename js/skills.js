f4211743bfd558d90258981b609150d7d641d10c
WeronikaMartinsen <121826608+WeronikaMartinsen@users.noreply.github.com>
2023-12-08 21:00:40 +0100

    added content

---
+++ js/skills.js
@@ -0,0 +1,8 @@
+const skills = document.querySelector(".skills");
+
+skills.innerHTML = `<div class="skills"><ion-icon name="logo-html5"></ion-icon>
+<ion-icon name="logo-nodejs"></ion-icon>
+<ion-icon name="logo-css3"></ion-icon>
+<ion-icon name="logo-wordpress"></ion-icon>
+<ion-icon name="logo-figma"></ion-icon></div>
+`;
