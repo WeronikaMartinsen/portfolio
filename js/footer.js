3a167232ed415b984504c02052f27fae2751dfb7
WeronikaMartinsen <121826608+WeronikaMartinsen@users.noreply.github.com>
2023-12-08 09:21:58 +0100

    working on footer

---
+++ js/footer.js
@@ -0,0 +1,25 @@
+const footer = document.querySelector(".footer");
+
+footer.innerHTML = `<div class="row">
+
+
+<div class="column"><img src="/images/martinsenLogo.png" alt="Logo">
+<a href=""><button class="button-main">CONTACT ME</button></a>
+</div>
+<div class="column">
+<ul>
+<li>Home</li>
+<li>Contact</li>
+<li>About</li>
+<li>Project</li>
+</ul>
+</div>
+<div class="column">
+<i class="fa-brands fa-github"></i>
+<i class="fa-brands fa-linkedin"></i>
+<i class="fa-brands fa-twitter"></i>
+
+</div>
+
+</div>
+ `;
