38afb0ca22eb658dc5c015f103de2847e2a463f0
WeronikaMartinsen <121826608+WeronikaMartinsen@users.noreply.github.com>
2023-12-13 21:25:25 +0100

    changes styling

--- js/contact.js
+++ js/contact.js
@@ -1,6 +1,6 @@
 const contact = document.querySelector(".contact");
 
-contact.innerHTML = `<div class="contact"><h3>Get in touch!</h3>
-<span>Feel free to reach out! I'm always open to new opportunities and collaborations.</span>
+contact.innerHTML = `<div class="contact"><h2 class="black">Get in touch!</h2>
+<span class="black">Feel free to reach out! I'm always open to new opportunities and collaborations.</span>
 <span class="black">wb3167@gmail.com</span>
 <button class="button-secondary no-border">Contact me</button></div>`;
