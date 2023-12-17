598361df32380c7d14bfb882dadc40963d5e8083
WeronikaMartinsen <121826608+WeronikaMartinsen@users.noreply.github.com>
2023-12-17 19:54:09 +0100

    fixed error

--- js/footer.js
+++ js/footer.js
@@ -9,7 +9,7 @@ footer.innerHTML = `<div class="row">
 <div class="column border-left">
 <ul>
 <li><a href="#home">Home</a></li>
-<li><a href="#contact">Contact</a></li>
+<li>Contact</li>
 <li><a href="#about">About</a></li>
 <li><a href="#project">Project</a></li>
 </ul>
